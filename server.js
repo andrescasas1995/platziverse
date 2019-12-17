d9ba8be45c19bafd38964eb2bbf60edd9bd8897f
Andres Casas <mrandres1995@gmail.com>
2019-12-16 23:28:25 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const debug = require('debug')('platziverse:api:routes')
+const http = require('http')
+const chalk = require('chalk')
+const express = require('express')
+const api = require('./api')
+
+const port = process.env.PORT || 3000
+const app = express()
+const server = http.createServer(app)
+
+app.use('/api', api)
+
+//Express error handle
+app.use((err, req, res, next) => {
+  debug(`Error: ${err.message}`)
+
+  if (err.message.match(/not found/)){
+    return res.status(404).send({ error: err.message })
+  }
+
+  res.status(500).send({ error: err.message})
+})
+
+function hundleFatalError (err) {
+    console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+}
+
+if (!module.parent) {
+    process.on('uncaughtException', hundleFatalError)
+    process.on('unhandledRejection', hundleFatalError)
+    
+    server.listen(port, () => {
+      console.log(`${chalk.green('[plazoverse-api]')} server listening on port ${port}`)
+    })
+}
+
+module.exports = server
